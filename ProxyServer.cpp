7f4adaf65e509754ef3864faf9ed3dedd92d735e
WildWind03 <usergreat03@gmail.com>
2016-11-30 23:57:29 +0700

    After 1 hour of work

--- ProxyServer.cpp
+++ ProxyServer.cpp
@@ -1,4 +1,23 @@
-//
-// Created by alexander on 30.11.16.
-//
+#include <sys/socket.h>
+#include "ProxyServer.h"
+
+ProxyServer::ProxyServer(int port) {
+    this -> port = port;
+    socketFd = socket(AF_INET, SOCK_STREAM, 0);
+
+    sockaddr sockaddr1;
+    sockaddr1.sa_data;
+    sockaddr1.sa_family;
+    bind(socketFd, sockaddr1, )
+}
+
+ProxyServer::~ProxyServer() {
+
+}
+
+void ProxyServer::start() {
+
+}
+
+
 
--- ProxyServer.h
+++ ProxyServer.h
@@ -1,8 +1,18 @@
-//
-// Created by alexander on 30.11.16.
-//
-
 #ifndef PROXY_PROXTSERVER_H
 #define PROXY_PROXTSERVER_H
+
+class ProxyServer {
+    int port;
+    int socketFd;
+
+public:
+    ProxyServer() = delete;
+    ProxyServer(const ProxyServer & proxyServer) = delete;
+    ProxyServer & operator= (const ProxyServer & p) = delete;
+    ProxyServer(int port);
+    void start();
+
+    virtual ~ProxyServer();
+};
 
 #endif //PROXY_PROXTSERVER_H
